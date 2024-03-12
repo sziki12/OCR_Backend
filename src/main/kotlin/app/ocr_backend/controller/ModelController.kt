d1e96cccc809f5f4076cb00c384ebb3d3cf86ad8
sziki12 <szikilevi01@gmail.com>
2024-03-12 11:30:15 +0100

    Response Charset Fixed
    UTF-8 now

--- src/main/kotlin/app/ocr_backend/controller/ModelController.kt
+++ src/main/kotlin/app/ocr_backend/controller/ModelController.kt
@@ -2,6 +2,7 @@ package app.ocr_backend.controller
 
 import app.ocr_backend.utils.PathHandler
 import java.io.File
+import java.nio.charset.StandardCharsets
 import java.util.*
 import kotlin.io.path.pathString
 
@@ -9,17 +10,20 @@ import kotlin.io.path.pathString
 class ModelController {
     private val execPath = PathHandler.getPythonDir().pathString+"${File.separator}Runnable.py"
     private val imagePath = PathHandler.getImageDir().pathString
-    //TODO Unreadable Characters maybe wrong charset?
     fun processImage(params:String):String
     {
         val processBuilder = ProcessBuilder("python",execPath,"--image",params,"--path",imagePath)
         processBuilder.redirectErrorStream(true)
+        processBuilder.environment()["PYTHONIOENCODING"] = "utf-8"
 
         val process = processBuilder.start()
-        val outScanner = Scanner(process.inputStream)
+        val outScanner = Scanner(process.inputStream, StandardCharsets.UTF_8)
         var out = ""
         while(outScanner.hasNextLine())
             out += outScanner.nextLine()+"\n"
+
+        println(out)
+        outScanner.close()
         return out
     }
 }
