e5aa5da10db97138828129929acdf4b0b68592fc
sziki12 <szikilevi01@gmail.com>
2024-04-14 22:06:49 +0200

    Cleanup and Bug fix
    Llama Python functional
    Item id not initialized error fixed
    Db set to update

--- src/main/python/Llama/LlamaRunnable.py
+++ src/main/python/Llama/LlamaRunnable.py
@@ -7,14 +7,8 @@ ap.add_argument("-rt", "--receiptText", required=True,
 ap.add_argument("-p", "--pathToModel", required=True,
 	help="The Path to the Llama model")
 
-
-test = "======\nADOSZAM: 11970529-2-44\n1784P-99X-S NOI BLUZ 7 Shi. (C08\n996AO-SEX-S NOI NADRAG gy COU\nZ0373-99X-M/L NOI FELSO 3 930 COU\n=-----=\n996AO-SEX-S NOI NADRAG gy COU\nZ0373-99X-M/L NOI FELSO 3 930 COU\nÖSSZESEN: 1 ) 585 Fi Ft\nBANKKÁRTYA: 20 589 Ft\n=-----=\nVYUGTASZAM: 0950/00045\n2024. 03. 14. he. 18\nNAV ELLENGRZ6 K6D:DF79C\nZP A23401039\n=-----="
-
-#args = vars(ap.parse_args())
-#llamaWrapper = ReceiptLlamaWrapper(args["pathToModel"])
-#llamaWrapper.textToReceiptJson(args["receiptText"])
-
-llamaWrapper = ReceiptLlamaWrapper("D:\\Llama\\Files\\Models\\llama2_7b\\llama-2-7b-chat.Q6_K.gguf")
-llamaWrapper.textToReceiptJson(test)
+args = vars(ap.parse_args())
+llamaWrapper = ReceiptLlamaWrapper(args["pathToModel"])
+llamaWrapper.textToReceiptJson(args["receiptText"])
 llamaWrapper.printResponse()
 
